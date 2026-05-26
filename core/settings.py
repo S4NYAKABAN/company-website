1ce1c70b47cf10642607672434aef507ec3e966b
Петровський Юрій <junior.4536.zx@gmail.com>
2026-05-27 00:08:40 +0300

    Allow all hosts in settings

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-94g3p_h0rgptbg6q-h=b2=giaz+&$!8k0bh(nu9%)2+5xxnd++
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
