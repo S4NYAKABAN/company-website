352740fbe884a6f16cc2448de7f696b64e6e5a1c
Артем Крамар <artem.kramar@knu.ua>
2026-05-26 19:12:29 +0300

    configure Django admin panel

--- company/admin.py
+++ company/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Service, Client, ContactMessage
 
-# Register your models here.
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')
+    search_fields = ('name', 'email')
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'sent_at')
+    readonly_fields = ('sent_at',)
